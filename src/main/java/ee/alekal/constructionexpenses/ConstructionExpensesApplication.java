5e8297f8d7360caa015c70441bc6c2ef0be95645
aleksale <aleksandr.aleksandrov@tietoevry.com>
2024-11-11 13:59:44 +0200

    initial commit

---
+++ src/main/java/ee/alekal/constructionexpenses/ConstructionExpensesApplication.java
@@ -0,0 +1,12 @@
+package ee.alekal.constructionexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionExpensesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConstructionExpensesApplication.class, args);
+    }
+}
